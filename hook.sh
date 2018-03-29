4d4a28ac87945290f69d7386fb4fd194832e3cfe
John Nguyen <nguyenj2@yahoo.com>
2018-03-28 21:46:13 -0700

    Updates

--- hook.sh
+++ hook.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$ACTION" = "download" ]; then
-    #echo "Attached pic" | mutt -a $ARGUMENT -s "Test Pic" -- nguyenj2@yahoo.com
+    #echo "Attached pic" | mutt -a $ARGUMENT -s "Test Pic" -- john@yahoo.com
     #gpicview $ARGUMENT &
-    #~/Dropbox-Uploader/dropbox_uploader.sh upload $ARGUMENT Jason_11th_Birthday &
+    #~/Dropbox-Uploader/dropbox_uploader.sh upload $ARGUMENT dest_dir
 
     fbi -T 2 -a $ARGUMENT
 fi
