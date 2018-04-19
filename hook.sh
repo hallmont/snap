cc716d35ea3f0b19ac42a5f689a6398a15329898
John Nguyen <nguyenj2@yahoo.com>
2018-04-18 23:34:16 -0700

    Get creds from separate file

--- hook.sh
+++ hook.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-GOOGLE_USER="myuser"
-GOOGLE_PASS="mypass"
-ALBUM="Test"
+. ~/snap/.google
 
 if [ "$ACTION" = "download" ]; then
     #echo "Attached pic" | mutt -a $ARGUMENT -s "Test Pic" -- john@yahoo.com
     #gpicview $ARGUMENT &
     #~/Dropbox-Uploader/dropbox_uploader.sh upload $ARGUMENT dest_dir
 
-    if [ "$GOOGLE_USER" != "myuser" ]; then
+    if [ "$GOOGLE_USER" != "" -a "$GOOGLE_USER" != "myuser" ]; then
         upload-gphotos $ARGUMENT -u "$GOOGLE_USER" -p "$GOOGLE_PASS" -a "$ALBUM" &
     fi
 
