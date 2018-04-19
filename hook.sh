7c78191b7be20a555cb545b9f4fef7721f53f4a2
John Nguyen <nguyenj2@yahoo.com>
2018-04-18 23:17:01 -0700

    Allow for upload to google photos

--- hook.sh
+++ hook.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+GOOGLE_USER="myuser"
+GOOGLE_PASS="mypass"
+ALBUM="Test"
+
 if [ "$ACTION" = "download" ]; then
     #echo "Attached pic" | mutt -a $ARGUMENT -s "Test Pic" -- john@yahoo.com
     #gpicview $ARGUMENT &
     #~/Dropbox-Uploader/dropbox_uploader.sh upload $ARGUMENT dest_dir
+
+    if [ "$GOOGLE_USER" != "myuser" ]; then
+        upload-gphotos $ARGUMENT -u "$GOOGLE_USER" -p "$GOOGLE_PASS" -a "$ALBUM" &
+    fi
 
     fbi -T 2 -a $ARGUMENT
 fi
